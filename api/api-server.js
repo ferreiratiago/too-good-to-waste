ea0fdb61582f198d9d7a64ca1f630ebd2a6b0ba9
Jose Novais <jpvnovais@gmail.com>
2016-10-07 22:23:46 +0100

    try to fix 'Access-Control-Allow-Methods'

--- api/api-server.js
+++ api/api-server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.use(methodOverride());
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
